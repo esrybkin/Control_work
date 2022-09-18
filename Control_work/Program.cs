e24abef71cb13fbba25767014fc3e97e411ea1fa
rybkin <erybkin@yandex.ru>
2022-09-18 13:25:36 +0700

    add reshenie zadachi

---
+++ Control_work/Program.cs
@@ -0,0 +1,13 @@
+﻿string[] arrstring = new string[] {"Hello", "2", "world", ":-)"};
+int count = 0;
+string[] arrtemp = new string[arrstring.Length];
+for (int i=0; i<arrstring.Length; i++)
+{
+    if (arrstring[i].Length<=3)
+    {
+        arrtemp[count] = arrstring[i];
+        count++;
+    }
+}
+Array.Resize(ref arrtemp, count);
+Console.WriteLine(string.Join(", ", arrtemp));
